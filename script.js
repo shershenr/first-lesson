9e052ec6bcbbcad1be25a7480bec7f5da8084c54
shershenr <mihalcukroman17@gmail.com>
2025-11-04 23:22:41 +0200

    first lesson fome work

---
+++ script.js
@@ -0,0 +1,43 @@
+/*
+let user = {
+    "name": "John",
+    "age": 32,
+    "work": true,
+};
+for (let key in user) {
+    console.log(key);
+    console.log(user[key]);
+};
+
+let codes = {
+    49:"Німечинна",
+    41:"Великобританія",
+    5:"США",
+}
+for (let code in codes){
+    console.log(code);
+}
+
+let a = "132asd";
+let b = "30qwd";
+let c = "14.4qwefa";
+
+console.log(typeof parseFloat(b));
+
+
+const message = "Zaharlox"
+console.log(message.toUpperCase())
+
+
+let user = {};
+user.username = "John";
+user.city = "New York";
+user.age = 7;
+delete user.city;
+user["like flovers"] = true;
+for (key in user) {
+    console.log(key);
+    console.log(user[key]);
+}
+*/
+
