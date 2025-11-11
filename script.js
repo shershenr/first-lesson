a255250027f897541e4ba42a1d7d0c568901ec3f
shershenr <mihalcukroman17@gmail.com>
2025-11-11 23:47:34 +0200

    full 5 lessons

--- script.js
+++ script.js
@@ -88,5 +88,212 @@ if (5 + 5 === 7) {
     console.log("no");
 }
 
- */
 
+let cost;
+const sub = "free";
+
+switch (sub) {
+    case "free":
+        cost = 0;
+        break;
+
+    case "pro":
+        cost = 100;
+        break;
+
+    case "Pemium":
+        cost = 200;
+        break;
+
+}
+console.log(cost);
+
+let cost;
+const cheats = "pro";
+
+ switch (cheats) {
+     case "lite":
+         cost = "5$";
+         break;
+
+     case "pro":
+         cost = "7$";
+         break;
+ }
+ console.log(cost);
+
+const min = prompt("Ввeдіть хвилини");
+
+if (min >=0 && min <= 14){
+    console.log("Перша чверть");
+} else if (min >= 15 &&  min <=29){
+    console.log("Друга чверть");
+} else if (min >= 30 && min <=44){
+    console.log("Третя чверть");
+} else if (min >= 45 && min <=59){
+    console.log("Четверта чверть");
+}
+
+const value = prompt("Введыть назву товару").toLowerCase();
+let cost;
+
+switch (value) {
+    case "телефон":
+        cost = 60000;
+        alert(`${value} коштує ${cost} гривень`)
+        break;
+    case "телевізор":
+        cost = 40000;
+        alert(`${value} коштує ${cost} гривень`)
+        break;
+    case "навушники":
+        cost = 8000;
+        alert(`${value} коштує ${cost} гривень`)
+        break;
+    case "мишка":
+        cost = 3000;
+        alert(`${value} коштує ${cost} гривень`)
+        break;
+
+    default: alert(`Toвару ${value},не має у наявності`)
+}
+
+let a = 0;
+
+while (a  < 5) {
+    a ++;
+    console.log("a", a);
+}
+
+let a = 2;
+do {
+    console.log(a)
+    a++
+}   while(a < 5);
+
+
+let a = 0;
+for ( ;a < 3; a++) {
+    console.log(a);
+}
+
+let summ = 0;
+
+while (true) {
+    let value = +prompt("Введіть число");
+    if (!value) break;
+
+    summ += value;
+
+}
+console.log("summ", summ);
+
+
+for (let i= 0 ; i < 10; i++){
+    if (i%2 === 0) continue
+    console.log(i);
+}
+
+let arr =["Apple","Orange","Plum"];
+
+arr [1] = "Lemon";
+
+arr [3] = "Cherry";
+
+
+arr.push("Tomat");
+
+
+
+arr.pop();
+
+arr.shift()
+
+arr.unshift("Tomat");
+
+console.log(arr);
+
+
+let arr =["Apple","Orange","Plum"];
+
+for (let item of arr){
+    console.log(item);
+}
+
+
+let matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9],
+];
+console.log(matrix[1][2]);
+
+
+let a = 5;
+let b = a;
+
+
+a = 10;
+console.log(a);
+console.log(b);
+
+let arr = [1,2,3];
+let arr2 = arr;
+
+arr.push(4);
+
+console.log(arr);
+
+
+let arr = ["a", "b", "c"];
+
+arr.splice(1,2);
+
+console.log(arr)
+
+
+let arr = ["a", "b", "c"];
+
+console.log(arr.includes ("b"));
+
+
+const fruits = [
+    { id: 0, name: "Apple"},
+    { id: 1, name: "Cherry"},
+    { id: 2, name: "Orange"},
+];
+
+//console.log(fruits.find((item) => item.id === 2));
+console.log(fruits.filter((item)=> item.id > 1));
+
+
+const fruits = [
+    { id: 0, name: "Apple"},
+    { id: 1, name: "Cherry"},
+    { id: 2, name: "Orange"},
+];
+
+let result = fruits.map((item) => item.name.lenght);
+console.log(result);
+
+
+
+let names = "Оля, Юля, Аня, Петя";
+
+let arr = names.split(", ");
+
+
+
+let newString = arr.join(", ");
+console.log(newString);
+
+let arr = [1,2,3,4,5]
+
+let red = arr.reduce((summ, item)=> summ+item)
+console.log(red)
+
+ */
+let arr = [23, 12, 23];
+
+let red = arr.reduce(( sum, item ) => sum + item) / arr.length;
+console.log(red);
